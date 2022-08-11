8556acc847599dca8868805deab41c597e341082
Mohammad Alfayoume <109729591+mohammadalfayoume@users.noreply.github.com>
2022-08-11 08:57:59 +0300

    Update ASAC-ch.js

--- Day13/demo/ASAC-CH/js/ASAC-ch.js
+++ Day13/demo/ASAC-CH/js/ASAC-ch.js
@@ -1,96 +1,150 @@
 "use strict";
-const body = document.getElementsByTagName("body");
-const allDrinks = [];
-let form = document.getElementById("form");
-//order of execution 
-function Drink(name, ingredients, image, iscold, ishot, price) {
+
+let allDrinks = [];
+
+function Drink(name, ingredients, image, isCold, isHot, price) {
   this.name = name;
   this.ingredients = ingredients;
   this.image = image;
-  this.isCold = iscold;
-  this.isHot = ishot;
+  this.isCold = isCold;
+  this.isHot = isHot;
   this.price = price;
+
   allDrinks.push(this);
 }
 
-Drink.prototype.printMenu = function () {
-  const divEl = document.createElement("div");
-  body[0].appendChild(divEl);
+let latte = new Drink(
+  "Latte",
+  ["milk", "ice", "sugar"],
+  "./assets/latte.png",
+  true,
+  true,
+  1
+);
+let mocha = new Drink(
+  "mocha",
+  ["milk", "coffee", "ice", "sugar"],
+  "./assets/mocha.png",
+  true,
+  false,
+  2
+);
+let hotChocalte = new Drink(
+  "hot chocalte",
+  ["milk", "coffee", "ice", "sugar"],
+  "./assets/mocha.png",
+  true,
+  false,
+  2
+);
 
-  const pEl = document.createElement("p");
-  pEl.textContent = `Drink name : ${this.name} : ${this.price}`;
-  divEl.appendChild(pEl);
+let sectionEl = document.getElementById("cardSection");
+Drink.prototype.render = function () {
+  // createing h3 for the name of the drink
+  let name = document.createElement("h3"); // <h3> </h3>
+  name.textContent = this.name; // <h3> the name of the drink </h3>
+  sectionEl.appendChild(name); // I can see the name inside the section
 
-  // const imgEl = document.createElement("img");
-  // imgEl.src = this.image;
-  // imgEl.alt = this.name;
-  // divEl.appendChild(imgEl);
+  // create the images :
+  let imageEl = document.createElement("img");
+  imageEl.src = this.image;
+  imageEl.style.width = "200px";
+  sectionEl.appendChild(imageEl);
 
-  // const ulEl = document.createElement("ul");
-  // divEl.appendChild(ulEl);
+  // price:
+  let price = document.createElement("p");
+  price.textContent = `${this.price} JD`;
+  sectionEl.appendChild(price);
+
+  // Ingredients: // array as list
+  let orderListEl = document.createElement("ol");
+  sectionEl.appendChild(orderListEl);
+
+  for (let i = 0; i < this.ingredients.length; i++) {
+    let list = document.createElement("li");
+    list.textContent = this.ingredients[i];
+    orderListEl.appendChild(list);
+  }
 };
 
-let americano = new Drink("americano", ["coffee", "ice", "sugar"],"./assets/americano.png", true,false,2.5);
-let espresso = new Drink("espresso",["milk", "coffee", "ice"],"./assets/espresso.png",true,false,2.5);
-let latte = new Drink("latte",["milk", "coffee", "ice", "sugar"],"./assets/latte.png",true,false,2.5);
-let mocha = new Drink("mocha",["coffee", "ice"],"./assets/mocha.png",true,false,2.5);
-console.log(1)
+let tableEl = document.getElementById("tableId");
 
-form.addEventListener("submit", handleSubmit);
-// functions
+Drink.prototype.renderTable = function () {
+  let tr = document.createElement("tr");
+  tableEl.appendChild(tr);
+
+  let nameTd = document.createElement("td");
+  nameTd.textContent = this.name;
+  tr.appendChild(nameTd);
+
+  let priceTd = document.createElement("td");
+  priceTd.textContent = this.price;
+  tr.appendChild(priceTd);
+};
+
+let formEl = document.getElementById("formId");
+formEl.addEventListener("submit", handleSubmit);
+
 function handleSubmit(event) {
-  console.log(3);
-  event.preventDefault();
-  let name = event.target.drinkName.value;
-  let ingredients = event.target.ingredients.value;
-  let img = event.target.img.value;
-  let price = event.target.price.value;
-  let cold = event.target.cold.checked;
-  let hot = event.target.hot.checked;
+  // the default behaviour of submitting the form is to refresh the page
 
-  const newDrink = new Drink(name, ingredients, img, cold, hot, price);
-  newDrink.printMenu();
+  event.preventDefault();
+  // for text input
+  let drinkName = event.target.drinkName.value;
+  let ingredients = event.target.ingredients.value;
+  let image = event.target.image.value;
+  let price = event.target.price.value;
+  // for checkbox input
+  let cold = event.target.cold.checked; // true or false
+  let hot = event.target.hot.checked;
+  let ingredientsArr = ingredients.split(",");
+
+  // create a new drink
+
+  const newDrink = new Drink(
+    drinkName,
+    ingredientsArr,
+    image,
+    cold,
+    hot,
+    price
+  );
+  newDrink.render();
+  newDrink.renderTable();
+
   saveData(allDrinks);
 }
 
 function saveData(data) {
-  console.log(4);
-  let stringObj = JSON.stringify(data);
-  localStorage.setItem("drinks", stringObj);
+  let stringfiyData = JSON.stringify(data);
+  localStorage.setItem("drinks", stringfiyData);
 }
 
 function getData() {
   let retrievedData = localStorage.getItem("drinks");
-  console.log(2);
 
   let arrayData = JSON.parse(retrievedData);
-
-  console.log(arrayData);
-// optional chaining
-  if (arrayData != null) {
-   
-    for (let i = allDrinks.length; i < arrayData.length; i++) {
-      // if (allDrinks[i]?.name !== arrayData[i].name) {
-        
+  // each object doesn't has access to render method
+  if (arrayData !== null) {
+    for (let i = 0; i < arrayData.length; i++) {
+      if (allDrinks[i]?.name !== arrayData[i].name) {
+        // reinstantiation: re creating instance
         new Drink(
           arrayData[i].name,
           arrayData[i].ingredients,
           arrayData[i].image,
-          arrayData[i].isCold,
+          arrayData[i].isCol,
           arrayData[i].isHot,
           arrayData[i].price
         );
-      // }
+      }
     }
   }
 
-
   for (let i = 0; i < allDrinks.length; i++) {
-    allDrinks[i].printMenu();
+    allDrinks[i].render();
+    allDrinks[i].renderTable();
   }
-
-  console.log("from constructor", allDrinks);
-  console.log("from LS", arrayData);
 }
 
 getData();
